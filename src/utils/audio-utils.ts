19e549e939a5552ebc7c0d5b261335e244894a39
Dakota Bourne <dakota.bourne0210@gmail.com>
2025-01-18 04:21:02 -0600

    fixed pathing of cookies

--- src/utils/audio-utils.ts
+++ src/utils/audio-utils.ts
@@ -1,12 +1,15 @@
 import ytdl from '@distube/ytdl-core';
 import fs from 'node:fs';
+import path from 'node:path';
 import internal from 'node:stream';
 
 import { Logger } from '../services/logger.js';
 
 export const getYoutubeAudio = (url: string): internal.Readable => {
     try {
-        const agent = ytdl.createAgent(JSON.parse(fs.readFileSync('cookies.json', 'utf8')));
+        const agent = ytdl.createAgent(
+            JSON.parse(fs.readFileSync(path.join(__dirname, 'cookies.json'), 'utf8'))
+        );
         return ytdl(url, {
             quality: 'highestaudio',
             filter: 'audioonly',
