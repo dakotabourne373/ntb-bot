e75316772f35f9a1c33d8a29ec9b8a22462661fc
Dakota Bourne <dakota.bourne0210@gmail.com>
2025-01-18 03:50:41 -0600

    add cookie support

--- src/utils/audio-utils.ts
+++ src/utils/audio-utils.ts
@@ -1,12 +1,27 @@
-import internal from 'node:stream';
 import ytdl from '@distube/ytdl-core';
+import fs from 'node:fs';
+import internal from 'node:stream';
 
-export const getYoutubeAudio = (url: string): internal.Readable =>
-    ytdl(url, {
-        quality: 'highestaudio',
-        filter: 'audioonly',
-        highWaterMark: 1 << 25,
-    });
+import { Logger } from '../services/logger.js';
+
+export const getYoutubeAudio = (url: string): internal.Readable => {
+    try {
+        const agent = ytdl.createAgent(JSON.parse(fs.readFileSync('cookies.json', 'utf8')));
+        return ytdl(url, {
+            quality: 'highestaudio',
+            filter: 'audioonly',
+            highWaterMark: 1 << 25,
+            agent,
+        });
+    } catch (e) {
+        Logger.error('out-of-date-cookies', e);
+        return ytdl(url, {
+            quality: 'highestaudio',
+            filter: 'audioonly',
+            highWaterMark: 1 << 25,
+        });
+    }
+};
 
 export const getYoutubeInfo = async (url: string): Promise<ytdl.videoInfo> =>
     await ytdl.getBasicInfo(url);
